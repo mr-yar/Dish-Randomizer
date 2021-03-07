6d263a4f0d540ae674d63aa079d88bc128497294
Nikita Yaremenko <mr.yaremenko.web@gmail.com>
2021-03-07 19:09:12 +0200

    Reorganized file structure, fixed minor bugs

--- src/redux/actions/actions.tsx
+++
@@ -1,7 +0,0 @@
-import {Meal} from '../../components/Interfaces';
-
-export function buttonLoad(
-  value: Meal
-): {payload: Meal; type: string} {
-  return {type: 'BUTTON_LOAD', payload: value};
-}
--- src/redux/reducers/rootReducer.tsx
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux';
-import {buttonReducer} from './buttonReducer';
-
-export const rootReducer = combineReducers({buttonReducer});
-export type RootState = ReturnType<typeof rootReducer>;
-
-
